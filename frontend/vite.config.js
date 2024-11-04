b3f24dd8c3dfaf6bd876ff760c7ba941cb8b2830
ankyson23 <aniket.sonawane1734@gmail.com>
2024-11-04 18:03:12 +0530

    setup vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/",
     server: {
         host: true, // This makes the server listen on all addresses
         port: 5173, // Optional: Specify a port if you want
